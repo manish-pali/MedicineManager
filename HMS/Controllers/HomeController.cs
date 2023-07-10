5cf9b312a6d609b45114076f2fa1a432b542128c
manish-pali <manishpali1588@gmail.com>
2023-07-11 01:19:23 +0530

    captcha removed

--- HMS/Controllers/HomeController.cs
+++ HMS/Controllers/HomeController.cs
@@ -25,43 +25,14 @@ namespace HMS.Controllers
      {
             try
             {
-                var ip = HttpContext.Request.UserHostAddress.ToString();
-                Session["ip"] = ip;
+               
                 var data = db.Tbl_Master_User.Where(x => x.UserName == model.username).FirstOrDefault();
                 if (data != null)
                 {
                     if (data.Password == model.password)
                     {
-                        if (Session["captcha"].ToString().ToLower() == model.cap.ToLower())
-                        {
-                            if (model.username == "Admin" && model.password == "1234")
-                            {
-                                Session["Session"] = "2023-24";
                                 Session["UserName"] = data.UserName;
-                                Session["Uid"] = data.Id;
-                                CustomClasses.UserLog(false, "AdminLogin", "Login|PageLoad", "Successful", Convert.ToString(model.username));
                                 return RedirectToAction("Dashboard", "Admin");
-                            }
-                            else
-                            {
-                                if (data.Ip == "Global")
-                                {
-                                    Session["Session"] = "2023-24";
-                                    Session["UserName"] = data.UserName;
-                                    Session["Uid"] = data.Id;
-                                    CustomClasses.UserLog(false, "AdminLogin", "Login|PageLoad", "Successful", Convert.ToString(model.username));
-                                    return RedirectToAction("Dashboard", "Admin");
-                                }
-                                else
-                                {
-                                    ViewBag.msg = "Your Ip is Incorrect";
-                                }
-                            }
-                        }
-                        else
-                        {
-                            ViewBag.msg = "Your Capture is Incorrect";
-                        }
                     }
                     else
                     {
